eb51f714a5d8963de0c66bdeca90cda476185d0d
jeremyfelt <jeremy.felt@gmail.com>
2015-11-05 16:46:20 -0800

    Provide inline JS and CSS to control show/hide behavior

--- wsu-show-and-hide.php
+++ wsu-show-and-hide.php
@@ -39,6 +39,8 @@ class WSU_Show_And_Hide {
 		add_action( 'init', array( $this, 'add_post_type_support' ) );
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
 		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+		add_action( 'wp_head', array( $this, 'wp_head' ), 10 );
+		add_action( 'wp_footer', array( $this, 'wp_footer' ), 10 );
 	}
 
 	/**
@@ -136,6 +138,119 @@ class WSU_Show_And_Hide {
 		$behavior['show_hide_change'] = sanitize_html_class( $_POST['show_hide_change'] );
 
 		update_post_meta( $post_id, '_wsuwp_show_hide_options', $behavior );
+	}
+
+	/**
+	 * Output the inline stylesheet used to control show/hide behavior.
+	 */
+	public function wp_head() {
+		if ( ! is_singular() ) {
+			return;
+		}
+
+		$post = get_post();
+
+		if ( ! post_type_supports( $post->post_type, 'wsuwp-show-and-hide' ) ) {
+			return;
+		}
+
+		$behavior_defaults = array(
+			'show_hide_click' => '',
+			'show_or_hide' => 'show',
+			'show_hide_change' => '',
+		);
+		$behavior = get_post_meta( $post->ID, '_wsuwp_show_hide_options', true );
+		$behavior = wp_parse_args( $behavior, $behavior_defaults );
+
+		// If classes haven't been assigned, skip everything.
+		if ( empty( $behavior['show_hide_click'] ) || empty( $behavior['show_hide_change'] ) ) {
+			return;
+		}
+
+		if ( ! in_array( $behavior['show_or_hide'], array( 'show', 'hide' ) ) ) {
+			$behavior['show_or_hide'] = 'show';
+		}
+
+		if ( 'show' === $behavior['show_or_hide'] ) {
+			?>
+			<style type="text/css">
+				.<?php echo sanitize_html_class( $behavior['show_hide_click'] ); ?> {
+					cursor: pointer;
+				}
+				.<?php echo sanitize_html_class( $behavior['show_hide_change'] ); ?> {
+					display: none !important;
+				}
+				.<?php echo sanitize_html_class( $behavior['show_hide_change'] ); ?>.show-hide-open {
+					display: inherit !important;
+				}
+			</style>
+			<?php
+		} else {
+			?>
+			<style type="text/css">
+				.<?php echo sanitize_html_class( $behavior['show_hide_click'] ); ?> {
+					cursor: pointer;
+				}
+				.<?php echo sanitize_html_class( $behavior['show_hide_change'] ); ?>.show-hide-close {
+					display: none !important;
+				}
+			</style>
+			<?php
+		}
+	}
+
+	/**
+	 * Output the inline JavaScript used to control the show/hide behavior.
+	 */
+	public function wp_footer() {
+		if ( ! is_singular() ) {
+			return;
+		}
+
+		$post = get_post();
+
+		if ( ! post_type_supports( $post->post_type, 'wsuwp-show-and-hide' ) ) {
+			return;
+		}
+
+		$behavior_defaults = array(
+			'show_hide_click' => '',
+			'show_or_hide' => 'show',
+			'show_hide_change' => '',
+		);
+		$behavior = get_post_meta( $post->ID, '_wsuwp_show_hide_options', true );
+		$behavior = wp_parse_args( $behavior, $behavior_defaults );
+
+		// If classes haven't been assigned, skip everything.
+		if ( empty( $behavior['show_hide_click'] ) || empty( $behavior['show_hide_change'] ) ) {
+			return;
+		}
+
+		if ( ! in_array( $behavior['show_or_hide'], array( 'show', 'hide' ) ) ) {
+			$behavior['show_or_hide'] = 'show';
+		}
+
+		?>
+		<script type="text/javascript">
+			(function($) {
+				var show_hide_click = $( ".<?php echo esc_attr( $behavior['show_hide_click'] ); ?>" ),
+					show_hide_change = $( ".<?php echo esc_attr( $behavior['show_hide_change'] ); ?>"),
+					show_or_hide = '<?php echo esc_js( $behavior['show_or_hide'] ); ?>';
+
+				var default_state = 'show' === show_or_hide ? 'show-hide-open' : 'show-hide-close';
+
+				show_hide_click.on( "click", function(e) {
+					e.preventDefault();
+
+					if ( show_hide_change.hasClass( default_state ) ) {
+						show_hide_change.removeClass( default_state );
+					} else {
+						show_hide_change.addClass( default_state );
+					}
+				});
+			}(jQuery));
+		</script>
+		<?php
 	}
 
 }
